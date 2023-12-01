509a95c7881eef1648b879fd01f8b7ef32781bfa
ARNOLD TWALA <arnoldtwl@gmail.com>
2023-12-01 19:18:16 +0200

    Add font styles and update headings in home and contact pages

--- app/page.js
+++ app/page.js
@@ -3,18 +3,19 @@ import Link from "next/link";
 import Header from "@/app/components/Header";
 import styles from "./page.module.css";
 import Image from "next/image";
+import { oswald, merriweatherSans } from "@/app/ui/fonts";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <Header />
       <section className={styles.hero}>
-        <h1>Welcome to My Developer Portfolio</h1>
-        <p className={styles.p}>
+        <h1 className={oswald.className}>Welcome to My Developer Portfolio</h1>
+        <p className={`${styles.p} ${merriweatherSans.className}`}>
           Discover more about me, my skills, and the projects I&apos;ve worked
           on.
         </p>
-        <div className={styles["cta-buttons"]}>
+        <div className={`${styles["cta-buttons"]} ${merriweatherSans.className}`}>
           <Link href="/portfolio/about" className={styles["cta-button"]}>
             About Me
           </Link>
@@ -28,14 +29,14 @@ export default function Home() {
         {/* Optional: Add image or illustration */}
       </section>
       <div className={styles.imageWrapper}>
-          <Image 
-            src="/homepage.png" 
-            alt="Homepage image" 
-            priority={true}
-            quality={100}
-            fill={true}
-          />
-        </div>
+        <Image
+          src="/homepage.png"
+          alt="Homepage image"
+          priority={true}
+          quality={100}
+          fill={true}
+        />
+      </div>
     </main>
   );
 }
--- app/portfolio/contact/page.js
+++ app/portfolio/contact/page.js
@@ -1,19 +1,29 @@
 // app\portfolio\contact\page.js
 import styles from './contact.module.css';
+import { oswald, merriweatherSans } from '@/app/ui/fonts';
 
 // Contact section
 export default function Contact() {
     return (
-        <div>
-            <section className={styles.section}>
-                <h1 >Contact</h1>
-                <p className={styles.p}>If you&apos;d like to get in touch, please use the details below:</p>
-                <ul className={styles.ul}>
-                    <li className={styles.li}>Email: <a href="mailto:arnoldtwl@gmail.com" className={styles.a}>arnoldtwl@gmail.com</a></li>
-                    <li className={styles.li}>Phone: 0727035123</li>
-                    <li className={styles.li}>Location: Greater Giyani Municipality, Limpopo, ZA</li>
-                </ul>
-            </section>
-        </div>
-    )
+      <div>
+        <section className={styles.section}>
+          <h1 className={oswald.className}>Contact</h1>
+          <p className={`${styles.p} ${merriweatherSans.className}`}>
+            If you&apos;d like to get in touch, please use the details below:
+          </p>
+          <ul className={`${styles.ul} ${merriweatherSans.className}`}>
+            <li className={styles.li}>
+              Email:{" "}
+              <a href="mailto:arnoldtwl@gmail.com" className={styles.a}>
+                arnoldtwl@gmail.com
+              </a>
+            </li>
+            <li className={styles.li}>Phone: 0727035123</li>
+            <li className={styles.li}>
+              Location: Greater Giyani Municipality, Limpopo, ZA
+            </li>
+          </ul>
+        </section>
+      </div>
+    );
 }
