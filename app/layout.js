d87b6275fd578809a5a769c33e83097d9ae6af36
ARNOLD TWALA <arnoldtwl@gmail.com>
2024-07-03 19:36:49 +0200

    chore: Update dependencies and import statements in layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,8 @@
 // app\layout.js
 import './globals.css'
 import { Inter } from 'next/font/google'
-import { Analytics } from '@vercel/analytics'
-import { SpeedInsights } from '@vercel/speed-insights/next'
+import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({ subsets: ['latin'] })
 
